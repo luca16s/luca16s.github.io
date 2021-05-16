04157a872a5051c900de9606e98b3699f28ba835
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-05-15 21:23:59 -0300

    - Incluir nova forma de se mostrar a foto
    - Incluir componente de profile
    - Incluir componente de Work-Experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      WorkExperienceComponent,
+      ProfileComponent
    ],
   imports: [
     BrowserModule,
@@ -26,6 +32,8 @@ import { MatCardModule } from '@angular/material/card';
     MatGridListModule,
     MatDividerModule,
     MatCardModule,
+    MatListModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
