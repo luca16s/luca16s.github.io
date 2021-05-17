3f42b2a4b2c381bb13a59feae82c916c6f1315da
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-05-17 00:10:49 -0300

    - Finalizar dados de Work XP
    Incluir todas as experiencias de trabalho

--- src/app/components/work-experience/work-experience.component.ts
+++ src/app/components/work-experience/work-experience.component.ts
@@ -24,12 +24,36 @@ export class WorkExperienceComponent implements OnInit {
 
   WorkExperienceList: WorkExperienceModel[] = [
     {
-      date_range: '06/2018 - Present',
-      location: 'Rio de Janeiro, Rio de Janeiro - Brazil',
+      date_range: 'From 06/2018 - Present',
+      location: 'Rio de Janeiro - Brazil',
       position: 'Software Developer',
       employee_name: 'Instituto Tecgraf/PUC-Rio',
-      employee_website: '',
+      employee_website: 'https://www.tecgraf.puc-rio.br/',
       job_description: "Developing .NET Framework applications with WinForms.\nI've started using Subversion as the main code versioning tool, and later helped migrate codebase to git.\nMore precisely, I act as a developer in a decision support system for the Oil and Gas industry, where the main focus is to optimize the allocation of Offshore Drilling Rigs and PLSV (Pipe-laying ships) to deep water exploration camps.",
+    },
+    {
+      date_range: 'From 10/2017 to 06/2018',
+      location: 'Rio de Janeiro - Brazil',
+      position: 'Trainee SAP - ABAP',
+      employee_name: 'Indra Company',
+      employee_website: 'https://www.indracompany.com/',
+      job_description: 'Developing new solutions, bug fixes, and documentation at Petrobras.\nVerifying the process of transporting modifications from development environment to production environment.\nSAP NetWeaver Portal Management and ABAP application development.\nSAPUI5 application development with OData Service access.',
+    },
+    {
+      date_range: 'From 12/2015 to 09/2017',
+      location: 'Rio de Janeiro - Brazil',
+      position: 'Jr. Support Analyst',
+      employee_name: 'DLima Consultoria Contábil e Tributária LTDA - ME',
+      employee_website: '',
+      job_description: "User support, program guides creation.\nNetwork monitoring, computer maintaining, and backup routines.\nNASAJON Accounting systems support and monitoring.\nNF-e management, correcting and sending accessory obligations like SPED-ICMS, LEDF-DF, EDOC-PE.",
+    },
+    {
+      date_range: 'From 12/2015 to 09/2017',
+      location: 'Rio de Janeiro - Brazil',
+      position: 'IT Intern',
+      employee_name: 'DLima Consultoria Contábil e Tributária LTDA - ME',
+      employee_website: '',
+      job_description: "User support.\nNetwork monitoring, computer maintenance, and backup routines.",
     }
   ]
 }
