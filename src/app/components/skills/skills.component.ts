8b0a3f7653a04086f0d5f75d7c5af87b1ffce54a
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-05-17 20:17:13 -0300

    - Inserir tecnologias

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface SkillsList {
+  category: string;
+  skills: Skill[];
+}
+
 export interface Skill {
   name: string;
   progress: number;
@@ -16,20 +21,71 @@ export class SkillsComponent implements OnInit {
 
   ngOnInit() {
   }
-  panelOpenState = false;
+  panelOpenState = true;
 
-  SkillList: Skill[] = [
+  SkillList: SkillsList[] = [
     {
-      name: 'Portuguese',
-      progress: 100,
+      category: 'Programing Languages',
+      skills: [] = [
+        {
+          name: 'C#',
+          progress: 80,
+        },
+        {
+          name: 'Java',
+          progress: 50,
+        },
+        {
+          name: 'Python',
+          progress: 40,
+        },
+        {
+          name: 'ABAP',
+          progress: 50,
+        },
+        {
+          name: 'Javascript/Typescript',
+          progress: 30,
+        }
+      ]
     },
     {
-      name: 'English',
-      progress: 70,
+      category: 'FrameWorks',
+      skills: [] = [
+        {
+          name: '.NET Core',
+          progress: 70,
+        },
+        {
+          name: '.NET Framework',
+          progress: 80,
+        },
+        {
+          name: 'Angular',
+          progress: 30,
+        },
+        {
+          name: 'VueJS',
+          progress: 30,
+        },
+      ]
     },
     {
-      name: 'French',
-      progress: 10,
-    }
+      category: 'Databases',
+      skills: [] = [
+        {
+          name: 'SQL Server',
+          progress: 60,
+        },
+        {
+          name: 'Oracle Database',
+          progress: 80,
+        },
+        {
+          name: 'MongoDB',
+          progress: 40,
+        },
+      ]
+    },
   ]
 }
